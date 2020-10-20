a73b48d42659d86f448882ae44474528da13c60d
Rajesh Shenoy <rajeshtshenoy@gmail.com>
2020-10-20 22:34:55 +0100

    first commit

--- server.js
+++ server.js
@@ -8,21 +8,73 @@ const io = socket(server);
 
 app.use(express.static(path.join(__dirname, "build")));
 
-const rooms = {};
+var users = {};
+var sockets = {};
+var freeUser;
 
 io.on("connection", (socket) => {
-  socket.on("join room", (roomID) => {
-    if (rooms[roomID]) {
-      rooms[roomID].push(socket.id);
-    } else {
-      rooms[roomID] = [socket.id];
+  if (!users[socket.id]) {
+    sockets[socket.id] = socket;
+    users[socket.id] = { connectedTo: -1, previous: -1 };
+    console.log(socket.id, "  just connected");
+  }
+
+  socket.emit("yourID", socket.id);
+
+  randomMatch();
+
+  function randomMatch() {
+    let numUsers = Object.keys(users).length;
+    if (numUsers > 1) {
+      freeUser = findFreeUser(users);
+      console.log("FREE USER ID-->", freeUser);
+      if (freeUser) {
+        users[socket.id].connectedTo = freeUser;
+        users[freeUser].connectedTo = socket.id;
+        socket.emit("other user", freeUser);
+        sockets[freeUser].emit("other user", socket.id);
+      }
     }
-    const otherUser = rooms[roomID].find((id) => id !== socket.id);
-    if (otherUser) {
-      socket.emit("other user", otherUser);
-      socket.to(otherUser).emit("user joined", socket.id);
+  }
+
+  function conditionalMatch(socketid) {
+    console.log("ENTERED CONDITONAL MATCH WITH ID ", socketid);
+    let numUsers = Object.keys(users).length;
+    if (numUsers > 1) {
+      let freeUser = Object.entries(users).filter(
+        (x) => x[0] !== users[socketid].previous && x[0] !== socketid
+      )[0][0];
+      console.log("FREE USER-->", freeUser);
+      users[socketid].connectedTo = freeUser;
+      users[freeUser].connectedTo = socketid;
+      console.log("USERS AFTER SETTING CONNECTEDTO--->", users);
     }
-  });
+  }
+
+  function fixMatches() {
+    //FIND LESS THAN ONE IN CONNECTEDTO
+    connectionsArray = Object.entries(users).map((x) => x[1].connectedTo);
+    console.log(connectionsArray);
+    let keysArray = Object.keys(users);
+    console.log(keysArray);
+    unconnectedUser = keysArray.filter(
+      (x) => connectionsArray.indexOf(x) < 0
+    )[0];
+    console.log("UNCONNECTED USER", unconnectedUser);
+    //FIND THE ONE CONNECTED TO THE LESS THAN ONE CONNECTED TO
+    if(unconnectedUser) {
+      unconnectedUserEntry = Object.entries(users).filter(
+        (x) => x[0] === unconnectedUser
+      );
+      incorrectUser = unconnectedUserEntry.flat()[1].connectedTo;
+      //FIX THE CONNECTION OF THE CONNECTED TO
+      users[incorrectUser].connectedTo = unconnectedUser;
+
+      console.log("FIXED CONNECTIONS", users);
+    }
+  }
+
+  io.sockets.emit("allUsers", users);
 
   socket.on("offer", (payload) => {
     io.to(payload.target).emit("offer", payload);
@@ -35,6 +87,77 @@ io.on("connection", (socket) => {
   socket.on("ice-candidate", (incoming) => {
     io.to(incoming.target).emit("ice-candidate", incoming.candidate);
   });
+
+  socket.on("disconnect", () => {
+    socket.emit("user left");
+
+    console.log(socket.id, "Disconnected from server");
+
+    var connTo = users[socket.id].connectedTo;
+    if (sockets[connTo]) {
+      users[connTo].connectedTo = -1;
+      sockets[connTo].emit("user left");
+    }
+    delete users[socket.id];
+    delete sockets[socket];
+
+    console.log(
+      "USERS",
+      users,
+      "NUMBER OF USERS ---->",
+      Object.keys(users).length
+    );
+    console.log("==================================");
+  });
+
+  socket.on("next", () => {
+    //=======================================================
+    console.log("===========ON NEXT==============");
+    console.log(socket.id, "<----Peer clicked next");
+    console.log("USERS AFTER NEXT CLICKED--->", users);
+    //=======================================================
+    let connTo = users[socket.id].connectedTo;
+    users[connTo].connectedTo = -1;
+    users[connTo].previous = socket.id;
+    sockets[connTo].emit("next user");
+    users[socket.id].previous = connTo;
+    users[socket.id].connectedTo = -1;
+    console.log("SETTING USERS CONNECTED TO -1-->", users);
+    socket.emit("next user");
+    let unconnectedUsers = Object.entries(users).filter(
+      (x) => x[1].connectedTo === -1
+    );
+    console.log("UNCONNECTED USERS---->", unconnectedUsers);
+    unconnectedUsers.forEach((x) => conditionalMatch(x[0]));
+    // fixMatches(); 
+    io.sockets.emit("allUsers", users);
+
+    //========================================================
+
+    console.log("===========END OF ON NEXT===============");
+    //=========================================================
+  });
+
+  function findFreeUser(users) {
+    var freeUsers = Object.entries(users).filter(
+      (x) => x[0] !== socket.id && x[1].connectedTo === -1
+    );
+    console.log("USERS IN FREE USER FUNCTION", users);
+    console.log("FREEUSERS-->", freeUsers);
+    var randomNumber = parseInt(Math.random() * freeUsers.length);
+    console.log("RANDOM NUMBER....--->", randomNumber);
+    if (freeUsers.length > 0) var randomFreeUser = freeUsers[randomNumber];
+    console.log("FREEUSER RANDOM---->", randomFreeUser);
+    if (randomFreeUser) return randomFreeUser[0];
+    else return 0;
+  }
+
+  console.log(
+    "USERS---->",
+    users,
+    "NUMBER OF USERS ---->",
+    Object.keys(users).length
+  );
 });
 
 app.get("*", function (req, res) {
--- src/App.js
+++ src/App.js
@@ -1,19 +1,316 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import CreateRoom from "./routes/CreateRoom";
-import Room from "./routes/Room";
-import "./App.css";
+import React, { useRef, useEffect, useState } from "react";
+import io from "socket.io-client";
+import styled from "styled-components";
+import logo from "./logo.gif";
+
+import { createGlobalStyle } from "styled-components";
+const GlobalStyles = createGlobalStyle`
+ 
+  @import url('https://fonts.googleapis.com/css2?family=Ranchers&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300;700&display=swap');
+
+  body {
+    font-family: 'Ranchers', cursive;
+    color:aquamarine;
+      text-shadow: 2px 2px grey;
+      font-size:3rem;
+      font-weight:700;
+  } 
+
+ 
+`;
+
+const Container = styled.div`
+  min-height: 100%;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  font-family: "Open Sans Condensed", sans-serif;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 0;
+`;
+
+const Video = styled.video`
+  width: 50%;
+  height: 50%;
+  border: 1px solid grey;
+  border-radius: 10px;
+  margin-right: 20px;
+  margin-left: 20px;
+  margin-top: 20px;
+`;
+const Button = styled.button`
+  background-color: aquamarine;
+  color: grey;
+  border-radius: 15px;
+  height: 60px;
+  width: 120px;
+  margin-top: 10px;
+  font-family: "Open Sans Condensed", sans-serif;
+  font-size: 2rem;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+  height: 60px;
+`;
 
 function App() {
+  const [yourID, setYourID] = useState();
+  const [users, setUsers] = useState({});
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [stream, setStream] = useState();
+  const [caller, setCaller] = useState("");
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const userVideo = useRef();
+  const partnerVideo = useRef();
+  const peerRef = useRef();
+  const socket = useRef();
+  const otherUser = useRef();
+  const userStream = useRef();
+
+  const stun_turn = {};
+
+  useEffect(() => {
+    console.log("IN USE EFFECT");
+
+    socket.current = io.connect("/");
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: true })
+      .then((stream) => {
+        setStream(stream);
+        if (userVideo.current) {
+          userVideo.current.srcObject = stream;
+          userStream.current = stream;
+        }
+      });
+
+    socket.current.on("yourID", (id) => {
+      setYourID(id);
+      console.log("YOUR ID--->", id);
+    });
+    socket.current.on("allUsers", (users) => {
+      setUsers(users);
+      console.log("ALL USERS", users);
+    });
+
+    socket.current.on("other user", (otherUserId) => {
+      otherUser.current = otherUserId;
+      console.log("OTHER USER ---> ", otherUserId);
+    });
+
+    socket.current.on("offer", handleReceiveCall);
+
+    socket.current.on("answer", handleAnswer);
+
+    socket.current.on("ice-candidate", handleNewICECandidateMsg);
+
+    socket.current.on("user left", () => {
+      setReceivingCall(false);
+      setCaller("");
+      setCallAccepted(false);
+      peerRef.current.close();
+    });
+
+    socket.current.on("next user", () => {
+      setReceivingCall(false);
+      setCaller("");
+      setCallAccepted(false);
+      peerRef.current.close();
+    });
+  }, []);
+
+  function callUser(userID) {
+    setCaller(yourID);
+    console.log(yourID, "...CALLED ...", otherUser.current);
+    peerRef.current = createPeer(userID);
+    userStream.current
+      .getTracks()
+      .forEach((track) => peerRef.current.addTrack(track, userStream.current));
+  }
+
+  function createPeer(userID) {
+    console.log(
+      `NEW PEER CREATED WITH USERID PARAMETER ${userID} BY...`,
+      yourID
+    );
+    const peer = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: "stun:stun.stunprotocol.org",
+        },
+        {
+          urls: "turn:numb.viagenie.ca",
+          credential: "muazkh",
+          username: "webrtc@live.com",
+        },
+      ],
+    });
+    console.log("PEER ---> ", peer);
+    peer.onicecandidate = handleICECandidateEvent;
+    peer.ontrack = handleTrackEvent;
+    peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
+
+    return peer;
+  }
+
+  function handleNegotiationNeededEvent(userID) {
+    console.log("IN HANDLE NEGOTIATION NEEDED EVENT");
+
+    peerRef.current
+      .createOffer()
+      .then((offer) => {
+        console.log("OFFER CREATED--->", offer);
+        return peerRef.current.setLocalDescription(offer);
+      })
+      .then(() => {
+        const payload = {
+          target: userID,
+          caller: socket.current.id,
+          sdp: peerRef.current.localDescription,
+        };
+        console.log("PAYLOAD----->", payload);
+        socket.current.emit("offer", payload);
+      })
+      .catch((e) => console.log(e));
+  }
+
+  function handleReceiveCall(incoming) {
+    console.log(
+      "IN HANDLERECEIVECALL.....INCOMING ARGUMENT-->",
+      incoming,
+      "  INCOMING CALLER ...->",
+      incoming.caller
+    );
+    peerRef.current = createPeer();
+    const desc = new RTCSessionDescription(incoming.sdp);
+    peerRef.current
+      .setRemoteDescription(desc)
+      .then(() => {
+        userStream.current
+          .getTracks()
+          .forEach((track) =>
+            peerRef.current.addTrack(track, userStream.current)
+          );
+      })
+      .then(() => {
+        return peerRef.current.createAnswer();
+      })
+      .then((answer) => {
+        console.log("ANSWER ---> ", answer);
+        return peerRef.current.setLocalDescription(answer);
+      })
+      .then(() => {
+        const payload = {
+          target: incoming.caller,
+          caller: socket.current.id,
+          sdp: peerRef.current.localDescription,
+        };
+        socket.current.emit("answer", payload);
+
+        setReceivingCall(true);
+        setCaller(incoming);
+        console.log(
+          "RECEIVED OFFER. IN HANDLE RECEIVE CALL, ANSWER PAYLOAD--->",
+          payload
+        );
+      });
+  }
+
+  function handleAnswer(message) {
+    setReceivingCall(true);
+    const desc = new RTCSessionDescription(message.sdp);
+    peerRef.current.setRemoteDescription(desc).catch((e) => console.log(e));
+    console.log(
+      "IN HANDLE ANSWER...MESSAGE PARAMETER --->",
+      message,
+      ".....DESCRIPTION....",
+      desc
+    );
+  }
+
+  function handleICECandidateEvent(e) {
+    if (e.candidate) {
+      const payload = {
+        target: otherUser.current,
+        candidate: e.candidate,
+      };
+      socket.current.emit("ice-candidate", payload);
+      console.log("IN HANDLE ICE CANDIDATE.....->", payload);
+      console.log("handleicecandidateevent e.....->", e);
+    }
+  }
+
+  function handleNewICECandidateMsg(incoming) {
+    console.log("IN HANDLE NEW ICE CANDIDATE...INCOMING--->", incoming);
+
+    const candidate = new RTCIceCandidate(incoming);
+    peerRef.current.addIceCandidate(candidate).catch((e) => console.log(e));
+  }
+
+  function handleTrackEvent(e) {
+    setCallAccepted(true);
+    console.log(
+      "IN HANDLE TRACK EVENT..SETTING CALL ACCEPTED TO ..->",
+      callAccepted
+    );
+    console.log("IN HANDLE TRACK EVENT..e ..->", e);
+    partnerVideo.current.srcObject = e.streams[0];
+  }
+
+  let UserVideo;
+  if (stream) {
+    console.log("IN USERVIDEO ELEMENT..STREAM IS...--> ", stream);
+    UserVideo = <Video playsInline ref={userVideo} autoPlay />;
+  }
+
+  let PartnerVideo;
+  if (callAccepted) {
+    console.log("IN CALL ACCEPTED..---> CALL ACCEPTED IS--->", callAccepted);
+
+    PartnerVideo = <Video playsInline ref={partnerVideo} autoPlay />;
+  }
+
+  let incomingCall;
+  if (receivingCall) {
+    console.log("IN RECEIVING CALL---> RECEIVING CALL VALUE-->", receivingCall);
+    incomingCall = <Button onClick={() => next()}>Next</Button>;
+  }
+
+  function next() {
+    socket.current.emit("next", socket.id);
+  }
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={CreateRoom} />
-          <Route path="/room/:roomID" component={Room} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <Container>
+      <GlobalStyles />
+      <Row>
+        <Logo src={logo} />
+        Taugle
+      </Row>
+      <Row>
+        {UserVideo}
+
+        {PartnerVideo}
+      </Row>
+
+      <Row>
+        <Button onClick={() => callUser(users[yourID].connectedTo)}>
+          Connect
+        </Button>
+        &nbsp;{incomingCall}&nbsp;
+        {Object.keys(users).length}&nbsp; Users Online
+      </Row>
+    </Container>
   );
 }
 
